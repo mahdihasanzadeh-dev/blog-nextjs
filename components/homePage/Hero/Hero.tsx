701cc52124bca045716a834480e9359af9ab40f2
Mahdi Hasanzadeh <hasanzadehmahdi@gmail.com>
2021-12-13 10:14:23 +0330

    add Hero component

--- components/homePage/Hero/Hero.tsx
+++ components/homePage/Hero/Hero.tsx
@@ -1,8 +1,22 @@
+import Image from 'next/image'
+import classes from './Hero.module.css'
 const Hero:React.FC =()=> {
     return (
-        <div>
-            
-        </div>
+        <section className={classes.hero}>
+            <div className={classes.image}>
+                <Image 
+                    src="/images/site/mahdi.jpg" 
+                    alt="An image showing Mahdi" 
+                    width={300} 
+                    height={300}
+                />
+            </div>
+            <h1>Hi. I'm Mahdi</h1>
+            <p>
+                I blog about web devlopment - especially frontend library and frameworks
+                like React Or NextJs
+            </p>
+        </section>
     )
 }
 
