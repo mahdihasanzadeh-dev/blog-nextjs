2fa62c0dd7000053fe737fdfc8b23a113d1cc38a
Mahdi Hasanzadeh <hasanzadehmahdi@gmail.com>
2021-12-13 09:37:00 +0330

    setting up the core pages

---
+++ pages/contact/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const ContactPage: NextPage = () => {
+  return (
+    <div >
+      <Head>
+        <title>Contact Us</title>
+        <meta name="description" content="Contact us" />
+      </Head>
+    </div>
+  )
+}
+
+export default ContactPage
---
+++ pages/posts/[slug].tsx
@@ -0,0 +1,16 @@
+import type {NextPage} from 'next'
+import Head from 'next/head'
+
+const PostDetailPage :NextPage =()=> {
+    return (
+        <div>
+            <Head>
+                <title>All Posts</title>
+                <meta name="description" content="" />
+            </Head>
+            
+        </div>
+    )
+}
+
+export default PostDetailPage
