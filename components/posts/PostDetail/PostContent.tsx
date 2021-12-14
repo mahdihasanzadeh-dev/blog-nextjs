24de0b1fcf3f37d02144671946c2f7846332a5ce
Mahdi Hasanzadeh <hasanzadehmahdi@gmail.com>
2021-12-14 12:15:01 +0330

    rendering Image with Next Image Component from markdown

--- components/posts/PostDetail/PostContent.tsx
+++ components/posts/PostDetail/PostContent.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Image from 'next/image'
 import  ReactMarkdown  from 'react-markdown'
 import PostHeader from './PostHeader'
 import {PostInterface} from '../../../types'
 import classes from './PostContent.module.css'
+
 
 type PostContentProps = {
     post:PostInterface
@@ -10,10 +12,24 @@ type PostContentProps = {
 const  PostContent:React.FC<PostContentProps> =({post})=> {
     const {slug, image, title, content} = post
     const imagePath = `/images/posts/${slug}/${image}`
+    const customRenderers = {
+        img(image:any){
+            return(
+                <Image 
+                src={`/images/posts/${slug}/${image.src}`} 
+                alt={image.alt} 
+                width={300} 
+                height={200} 
+                layout="responsive"
+                 />
+            ) 
+        
+        },
+    }
     return (
         <article className={classes.content}>
             <PostHeader title={title} image={imagePath} />
-            <ReactMarkdown>
+            <ReactMarkdown components={customRenderers}>
                 {content}
             </ReactMarkdown>
             
